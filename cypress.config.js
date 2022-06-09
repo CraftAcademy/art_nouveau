542d57eaf93bb83e06187f9403ddf965d7d53ab6
Thomas Ochman <thomas@dragster.se>
2022-06-09 13:23:50 +0200

    updates formatting in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    chromeWebSecurity: false
+    baseUrl: "http://localhost:3000",
+    chromeWebSecurity: false,
   },
 });
