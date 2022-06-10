de25925d07a4ac90a745e94b47030259e896df22
maxarvid <max.arvid.anderson@gmail.com>
2022-06-10 11:30:54 +0200

    displays the fetched projects data on load

--- cypress/e2e/userSeesListOfProjects.cy.js
+++ cypress/e2e/userSeesListOfProjects.cy.js
@@ -1,16 +1,24 @@
 /* eslint-disable no-undef */
 describe("When a user visits the application", () => {
   beforeEach(() => {
-    // Add an intercept
-    cy.intercept('GET', '**/projects', { fixture: 'projectsIndex.json'}).as('projectsIndex')
+    cy.intercept("GET", "**/projects", { fixture: "projectsIndex.json" }).as(
+      "projectsIndex"
+    );
     cy.visit("/");
   });
 
-  it('is expected to make an API call', () => {
-    cy.wait('@projectsIndex').its('request.method').should('eql', 'GET')
+  it("is expected to make an API call", () => {
+    cy.wait("@projectsIndex").its("request.method").should("eql", "GET");
   });
-  
+
   it("is expected to display a list of projects", () => {
-    cy.get("[data-cy=projects-list]").should("have.length", 3);
+    cy.get("[data-cy=projects-list]").children().should("have.length", 3);
+  });
+
+  it.only("is expected to display the title of the first project", () => {
+    cy.get("[data-cy=projects-list]")
+      .children()
+      .first()
+      .should("contain.text", "3D disco");
   });
 });
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-
 const Projects = () => {
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState([]);
 
+  const fetchProjects = async () => {
+    const { data } = await axios.get("http://localhost:3001/projects");
+    setProjects(data.projects);
+  };
   useEffect(() => {
-    const fetchProjects = async () => {
-      const { data } = await axios.get('http://localhost:3001/projects')
-      setProjects(data.projects)
-    }
+    fetchProjects();
+  }, []);
 
-    fetchProjects()
-  }, [])
-  
-  return <div data-cy="projects-list">Hello Projects!</div>;
+  const projectsList = projects.map((project) => {
+    return (
+      <li key={project.id}>
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+      </li>
+    );
+  });
+
+  return <ul data-cy="projects-list">{projectsList}</ul>;
 };
 
 export default Projects;
