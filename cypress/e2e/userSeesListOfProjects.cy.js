d210736526242503174ab4dad36c81f58a5012c4
Thomas Ochman <thomas@dragster.se>
2022-06-10 11:19:07 +0200

    adds api call to fetch project data

--- cypress/e2e/userSeesListOfProjects.cy.js
+++ cypress/e2e/userSeesListOfProjects.cy.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-undef */
 describe("When a user visits the application", () => {
   beforeEach(() => {
     // Add an intercept
+    cy.intercept('GET', '**/projects', { fixture: 'projectsIndex.json'}).as('projectsIndex')
     cy.visit("/");
   });
 
+  it('is expected to make an API call', () => {
+    cy.wait('@projectsIndex').its('request.method').should('eql', 'GET')
+  });
+  
   it("is expected to display a list of projects", () => {
-    cy.get("[data-cy=projects-list]").should("have.length", 6);
+    cy.get("[data-cy=projects-list]").should("have.length", 3);
   });
 });
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import axios from "axios";
+
 
 const Projects = () => {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const { data } = await axios.get('http://localhost:3001/projects')
+      setProjects(data.projects)
+    }
+
+    fetchProjects()
+  }, [])
+  
   return <div data-cy="projects-list">Hello Projects!</div>;
 };
 
