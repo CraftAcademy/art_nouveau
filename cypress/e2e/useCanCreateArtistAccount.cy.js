2e800475a5aee9c060222f5ba0e7e89bad7b9602
maxarvid <max.arvid.anderson@gmail.com>
2022-06-30 11:55:59 +0200

    WIP adds failing test for url redirect on create project button click

---
+++ cypress/e2e/useCanCreateArtistAccount.cy.js
@@ -0,0 +1,35 @@
+describe("When user creates an artist account", () => {
+  beforeEach(() => {
+    cy.visitApplication();
+  });
+
+  describe("as an authenticated user", () => {
+    beforeEach(() => {
+      cy.applicationState().invoke("dispatch", {
+        type: "user/setCurrentUser",
+        payload: { name: "Thomas", email: "thomas@random.com" },
+      });
+      cy.get("[data-cy=create-project-btn]").click();
+    });
+    it("is expected to direct user to create project view", () => {
+      cy.url().should("include", "/projects/create");
+    });
+  });
+
+  describe("as an unauthenticated user", () => {
+    beforeEach(() => {
+      cy.get("[data-cy=create-project-btn]").click();
+    });
+    describe("successfully", () => {
+      // Expect url to go to login path
+      // Fill out create account form
+      // Send it off
+      // Expect message
+      // Expect url /create
+    });
+
+    describe("unsuccessfully", () => {
+      // Add intercept with 422 or similar
+    });
+  });
+});
